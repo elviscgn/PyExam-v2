53b6ab180085446e816cfe6e8d31d51fccb2f7df
Elvis <96030189+elviscgn@users.noreply.github.com>
2025-11-29 08:17:42 +0200

    Add theoretical questions for Python section A

---
+++ section_a.py
@@ -0,0 +1,50 @@
+"""
+SECTION A:
+_   _                           
+| | | |                          
+| |_| |__   ___  ___  _ __ _   _ 
+| __| '_ \ / _ \/ _ \| '__| | | |
+| |_| | | |  __/ (_) | |  | |_| |
+ \__|_| |_|\___|\___/|_|   \__, |
+                            __/ |
+                           |___/ 
+"""
+
+# To answer the theoretical questions just use print 
+# Do NOT consult the internet for any of these questions
+
+# 1. Explain the difference between expressions and statements in Python. Give one of each.
+
+# 2. Describe what mutability means. Give an example of a mutable type and an immutable type.
+
+# 3. What do you understand by the term "variable scope"
+
+# 4. WHat is the difference between local, global? Give examples.
+
+# 5. What is a function? Why is returning a value different from printing a value?
+
+# 6. Explain the difference between defining a function and calling a function.
+
+# 7. Explain the difference between for loops and while loops, and when to use each.
+
+# 8. Explain how Python evaluates if / elif / else.
+
+# 9. What is the difference between == and is?
+
+# 10. What is a list? How is it indexed?
+
+# 11. What does “mutable” mean? Give 3 mutable and 3 immutable types.
+
+# 12. What is a dictionary and when should you use one?
+
+# 13. Explain what an algorithm is.
+
+# 14. What is a module?
+
+# 15. What is TDD? Describe the 3 steps: Red -> Green -> Refactor.
+
+# 16. Explain the purpose of unittest in Python.
+
+# 17. What is the purpose of assertions in unit tests?
+
+# 18. Explain what an exception is and why we raise exceptions.
